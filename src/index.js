cff16be3440278192f5f1b79d6395c38c3fd1dfd
Jamie Berrier <berrier.jamie@gmail.com>
2020-06-08 19:32:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -38,6 +38,8 @@ function fetchDogBreeds() {
 
 // add the breeds to the page in an <ul> (take a look at the included index.html)
 function renderDogBreeds(breeds) {
+  removeChildren(ul)
+  
   for (breed of breeds) {
     const li = document.createElement('li')
 
