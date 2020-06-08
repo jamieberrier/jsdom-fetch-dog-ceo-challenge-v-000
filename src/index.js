3f6cc563d14451b5a17c437edffef1d8d7992128
Jamie Berrier <berrier.jamie@gmail.com>
2020-06-08 19:27:54 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 console.log('%c HI', 'color: firebrick')
 
+const ul = document.getElementById('dog-breeds')
+
 // on page load, fetch the images using the url, parse the response as JSON
 function fetchDogPics() {
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
@@ -33,7 +35,6 @@ function fetchDogBreeds() {
 function renderDogBreeds(breedsObject) {
   // the return value is an Array containing all of the keys at the top level of the Object
   const breeds = Object.keys(breedsObject)
-  const ul = document.getElementById('dog-breeds')
 
   for (breed of breeds) {
     const li = document.createElement('li')
