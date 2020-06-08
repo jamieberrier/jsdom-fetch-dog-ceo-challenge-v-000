46a7431d23d1c3d51e0e309f0bdae88ba0fbfdd8
Jamie Berrier <berrier.jamie@gmail.com>
2020-06-08 18:31:45 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 
 document.addEventListener('DOMContentLoaded', function() {
+  fetchDogs()
+})
+function fetchDogs() {
   fetch(imgUrl)
   .then(response => response.json())
   .then(json => renderDogs(json))
-})
+}
 
 function renderDogs(json) {
   const main = document.getElementById('dog-image-container')
