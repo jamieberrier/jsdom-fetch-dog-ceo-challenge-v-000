c7985afa2ab43ec5b7599d1b638547c82c627c09
Jamie Berrier <berrier.jamie@gmail.com>
2020-06-08 18:44:14 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -21,12 +21,3 @@ function renderDogs(dogPic) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchDogs()
 })
-/*
-function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
-*/
